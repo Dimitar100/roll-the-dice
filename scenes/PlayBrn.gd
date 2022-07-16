e971ed4e150ce95e17f2ce52306eda1ded8b5225
BuildTools <stoilovdimitar29@gmail.com>
2022-07-16 14:45:04 +0300

    started working on the UI of the game

---
+++ scenes/PlayBrn.gd
@@ -0,0 +1,9 @@
+extends TextureButton
+
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_PlayBrn_pressed():
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://scenes/SelectLevel.tscn")
