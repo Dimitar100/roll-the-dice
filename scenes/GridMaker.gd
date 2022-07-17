6726e0fcd42635757121d06c1f8f7c6380505cbb
BuildTools <stoilovdimitar29@gmail.com>
2022-07-17 19:06:18 +0300

    only the music left

--- scenes/GridMaker.gd
+++ scenes/GridMaker.gd
@@ -83,13 +83,12 @@ func turn_dice():
 	else:
 		count += 1
 	
-	if count == matrix[player[1]][player[0]]:
-		if get_parent().dice1+1 == count:
+	if count-1 == matrix[player[1]][player[0]]:
+		if get_parent().dice1+1 == count-1:
 			get_parent().get_node("dice1").play(get_parent().dots[get_parent().dice1+6])
-		if get_parent().dice2+1 == count:
+		if get_parent().dice2+1 == count-1:
 			get_parent().get_node("dice2").play(get_parent().dots[get_parent().dice2+6])	
 			
-				
 func _ready():
 	for x in range(width):
 		matrix.append([])
@@ -122,50 +121,68 @@ func check(var matrix_field):
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("ui_up"):
 		if player[0] != 0:
-			matrix_fields[player[1]][player[0]].visible = true
-			player[0] = player[0]-1
-			matrix_fields[player[1]][player[0]].visible = false
-			get_node("Player").position.y = get_node("Player").position.y - 80
+			#matrix_fields[player[1]][player[0]].visible = true
+			matrix[player[1]][player[0]] = -1
+			if matrix[player[1]][player[0]-1] != -1:
+				player[0] = player[0]-1
+				matrix_fields[player[1]][player[0]].visible = false
+				get_node("Player").position.y = get_node("Player").position.y - 80
+				get_node("Player").up_dots()
 		else:
-			matrix_fields[player[1]][player[0]].visible = true
-			player[0] = 6
-			matrix_fields[player[1]][player[0]].visible = false
-			get_node("Player").position.y = get_node("Player").position.y + 480
+			matrix[player[1]][player[0]] = -1
+			if matrix[player[1]][6] != -1:
+				player[0] = 6
+				matrix_fields[player[1]][player[0]].visible = false
+				get_node("Player").position.y = get_node("Player").position.y + 480
+				get_node("Player").up_dots()
 		turn_dice()
 	elif Input.is_action_just_pressed("ui_down"):
 		if player[0] != 6:
 			get_node("Player").can_up = true
-			matrix_fields[player[1]][player[0]].visible = true
-			player[0] = player[0]+1
-			matrix_fields[player[1]][player[0]].visible = false
-			get_node("Player").position.y = get_node("Player").position.y + 80
+			matrix[player[1]][player[0]] = -1
+			if matrix[player[1]][player[0]+1] != -1:
+				player[0] = player[0]+1
+				matrix_fields[player[1]][player[0]].visible = false
+				get_node("Player").position.y = get_node("Player").position.y + 80
+				get_node("Player").up_dots()
 		else:
-			matrix_fields[player[1]][player[0]].visible = true
-			player[0] = 0
-			matrix_fields[player[1]][player[0]].visible = false
-			get_node("Player").position.y = get_node("Player").position.y - 480
+			matrix[player[1]][player[0]] = -1
+			if matrix[player[1]][0] != -1:
+				player[0] = 0
+				matrix_fields[player[1]][player[0]].visible = false
+				get_node("Player").position.y = get_node("Player").position.y - 480
+				get_node("Player").up_dots()
 		turn_dice()
 	elif Input.is_action_just_pressed("ui_right"):
 		if player[1] != 8:
-			matrix_fields[player[1]][player[0]].visible = true
-			player[1] = player[1]+1
-			matrix_fields[player[1]][player[0]].visible = false
-			get_node("Player").position.x = get_node("Player").position.x + 80
+			matrix[player[1]][player[0]] = -1
+			if matrix[player[1]+1][player[0]] != -1:
+				player[1] = player[1]+1
+				matrix_fields[player[1]][player[0]].visible = false
+				get_node("Player").position.x = get_node("Player").position.x + 80
+				get_node("Player").up_dots()
 		else:
-			matrix_fields[player[1]][player[0]].visible = true
-			player[1] = 0
-			matrix_fields[player[1]][player[0]].visible = false
-			get_node("Player").position.x = get_node("Player").position.x - 640
+			matrix[player[1]][player[0]] = -1
+			if matrix[0][player[0]] != -1:
+				player[1] = 0
+				matrix_fields[player[1]][player[0]].visible = false
+				get_node("Player").position.x = get_node("Player").position.x - 640
+				get_node("Player").up_dots()
 		turn_dice()
 	elif Input.is_action_just_pressed("ui_left"):
 		if player[1] != 0:
-			matrix_fields[player[1]][player[0]].visible = true
-			player[1] = player[1]-1
-			matrix_fields[player[1]][player[0]].visible = false
-			get_node("Player").position.x = get_node("Player").position.x - 80
+			matrix[player[1]][player[0]] = -1
+			if matrix[player[1]-1][player[0]] != -1:
+				player[1] = player[1]-1
+				matrix_fields[player[1]][player[0]].visible = false
+				get_node("Player").position.x = get_node("Player").position.x - 80
+				get_node("Player").up_dots()
 		else:
-			matrix_fields[player[1]][player[0]].visible = true
-			player[1] = 8
-			matrix_fields[player[1]][player[0]].visible = false
-			get_node("Player").position.x = get_node("Player").position.x + 640
+			matrix[player[1]][player[0]] = -1
+			if matrix[8][player[0]] != -1:
+				player[1] = 8
+				matrix_fields[player[1]][player[0]].visible = false
+				get_node("Player").position.x = get_node("Player").position.x + 640
+				matrix[player[1]][player[0]] = -1
+				get_node("Player").up_dots()
 		turn_dice()
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -14,15 +14,12 @@ var can_down = true
 var can_right = true
 var can_left = true
 
-func rotate_right(dot):#right 45 deg
-	dot += 90
-	if dot == 360:
-		dot = 0
-
-func rotate_left(dot):#left 45 deg
-	dot -= 90
-	if dot < 0:
-		dot = 270
+func up_dots():#left 45 deg
+	if i == 5:
+		i = 0
+	else:
+		i += 1
+	get_node("AnimatedSprite").play(dots[i])
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -31,30 +28,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
-	if Input.is_action_just_pressed("ui_up") && can_up:
-		if i == 5:
-			i = 0
-		else:
-			i += 1
-		get_node("AnimatedSprite").play(dots[i])
-	elif Input.is_action_just_pressed("ui_down") && can_down:
-		if i == 5:
-			i = 0
-		else:
-			i += 1
-		get_node("AnimatedSprite").play(dots[i])
-	elif Input.is_action_just_pressed("ui_right") && can_right:
-		if i == 5:
-			i = 0
-		else:
-			i += 1
-		get_node("AnimatedSprite").play(dots[i])
-	elif Input.is_action_just_pressed("ui_left") && can_left:
-		if i == 5:
-			i = 0
-		else:
-			i += 1
-		get_node("AnimatedSprite").play(dots[i])
+	pass
 		
 	
 
